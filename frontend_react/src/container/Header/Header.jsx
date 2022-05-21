7ba37f145cac9c46905478afb2d9b6ddf7bbd548
stephen <nwabuezesc@gmail.com>
2022-05-21 11:25:14 +0100

    navbar setup

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constant'
 import './Header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">Micael</h1>
+          </div>
+        </div>
+
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Web Developer</p>
+          <p className="p-text">Freelancer</p>
+        </div>
+      </div>
+    </motion.div>
+    </div>
   )
 }
 
